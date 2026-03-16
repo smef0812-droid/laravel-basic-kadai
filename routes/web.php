188190af40684ce7bff24e7e08f9f746fd55f6b6
smef0812-droid <smef0812@gmail.com>
2026-03-16 23:20:56 +0900

    hosting

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PostController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello, world!';
+});
+
+Route::get('/posts',[PostController::class, 'index']);
